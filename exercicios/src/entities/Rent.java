4ac6fcf81a0ed7ba7645201f02db2ab24540e800
Mario Salles <mariofsalles@gmail.com>
2022-08-07 01:11:02 +0100

    Fixed conflicts Pessoa class

--- exercicios/src/entities/Rent.java
+++ exercicios/src/entities/Rent.java
@@ -10,22 +10,6 @@ public class Rent {
 		this.email = email;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	@Override
 	public String toString() {
 		return name + ", " + email;
