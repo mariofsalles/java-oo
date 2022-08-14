89ba9836f4c0d8ffc37ebe21e0bf8bdf872e5fe8
Mario Salles <mariofsalles@gmail.com>
2022-08-14 18:55:24 +0100

    Added instaceof test for downcasting

---
+++ heranca/src/entities/BusinessAccount.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+	private Double loanLimit;
+
+	public BusinessAccount() {
+	}
+	
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+	
+	public void loan(double value) {
+		if (value <= loanLimit) {
+			balance += value - 10.0;			
+		}
+	}
+}
