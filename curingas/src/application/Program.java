759a2b13d8895aa8314197f3a95cdc4b775882a2
Mario Salles <mariofsalles@gmail.com>
2022-08-27 23:51:04 +0100

    Added wild cards examples

---
+++ curingas/src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Maria", "Joao", "Alex");
+		printList(myStrs);
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
+	}
+
+}
