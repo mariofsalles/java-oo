6150f7ff0bdb832fd98d9b9753e0f05e7e43d267
Mario Salles <mariofsalles@gmail.com>
2022-08-28 20:37:19 +0100

    Added hashcode and equals examples

---
+++ hashCode-equals/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import entities.Client;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
+		String s1 = new String("Test");
+		String s2 = new String("Test");
+
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c1 == c2);
+		System.out.println("-----------");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1.equals(s2));
+		System.out.println(s1 == s2);
+
+	}
+
+}
